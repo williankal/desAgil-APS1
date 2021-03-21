3b0e4d75af9b69513510e793834dfc933b33848c
Willian Kenzo <kenzo.willian@hotmail.com>
2021-03-20 23:02:54 -0300

    teste

--- VaiMalte/src/br/edu/insper/desagil/asp1/Carrinho.java
+++ VaiMalte/src/br/edu/insper/desagil/asp1/Carrinho.java
@@ -1,7 +1,18 @@
 package br.edu.insper.desagil.asp1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carrinho {
-	private List<Pedido> pedidos;
 	
+	private List<Pedido> pedidos;
+
+	public Carrinho() {
+		this.pedidos = new ArrayList<>();
+	}
+	public List<Pedido> getPedidos() {
+		return this.pedidos;
+	}
+	public void 
 
 }
